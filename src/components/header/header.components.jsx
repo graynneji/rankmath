1feaadef88e15b1eeba58520b29e3d265b8bf5a3
Ukaegbu Gray <graynneji@outlook.com>
2023-07-23 17:13:08 +0200

    commit

--- src/components/header/header.components.jsx
+++ src/components/header/header.components.jsx
@@ -39,21 +39,21 @@ const Header = () => {
           >
             <ul className="mobile-list">
               <li className="hc-border-bottom">
-                <span>Edit</span>
+                <span className="lists">Edit</span>
                 <CgEditFlipH style={{ gridColumn: -1 }} size={20} />
               </li>
               <li className="hc-border-bottom">
-                <span>Courier info</span>
+                <div className="lists">Courier info</div>
                 {/* <div className="p"> */}
                 <CiSquareInfo style={{ gridColumn: -1 }} size={20} />
                 {/* </div> */}
               </li>
               <li className="hc-border-bottom">
-                <span style={{ gridColumn: 1 / 2 }}>Share</span>
+                <span className="lists">Share</span>
                 <PiShareDuotone style={{ gridColumn: -1 }} size={20} />
               </li>
               <li className="hc-border-bottom" onClick={handleToggleMenu}>
-                <span>Remove</span>
+                <span className="lists">Remove</span>
                 <CiCircleRemove style={{ gridColumn: -1 }} size={20} />
               </li>
             </ul>
